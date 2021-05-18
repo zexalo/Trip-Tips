89b4473c14134220ae5d361cff08846abda6af77
Felixned <55989975+Felixned@users.noreply.github.com>
2021-05-18 14:07:17 +0200

    form validation clientside for login and register

--- frontend/src/accueil/loginAndRegister/login.jsx
+++ frontend/src/accueil/loginAndRegister/login.jsx
@@ -1,11 +1,48 @@
 import React from "react";
 import logoTripTips from "../../images/logoTripTips.png";
 
+
+const formValid = ({ ...rest }) => {
+    let valid = true;
+    // validate the form was filled out
+    Object.values(rest).forEach(val => {
+      val === null && (valid = false);
+    });
+  
+    return valid;
+};
+
 export class Login extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            email: null,
+            password: null,
+        };
     }
+
+    handleChange = e => {
+        e.preventDefault();
+        const { name, value } = e.target;
+        this.setState({ [name]: value } );
+    };
+
+    
+    handleSubmit = e => {
+        e.preventDefault();
+    
+        if (formValid(this.state)) {
+          console.log(`
+            --SUBMITTING--
+            Email: ${this.state.email}
+            Password: ${this.state.password}
+          `);
+        } else {
+          console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+        }
+    };
 
     render() {
         return <div className="base-container" ref={this.props.containerRef}>
@@ -14,20 +51,34 @@ export class Login extends React.Component {
             </div>
             <div className="header">Login</div>
             <div className="content">
-                <form className="form">
+                <form className="form" onSubmit={this.handleSubmit} noValidate>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="e-mail" placeholder="email"></input>
+                        <input 
+                            type="email" 
+                            name="email" 
+                            placeholder="email"
+                            noValidate
+                            onChange={this.handleChange}
+                        ></input>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="password"></input>
+                        <input 
+                            type="password" 
+                            name="password" 
+                            placeholder="password"
+                            noValidate
+                            onChange={this.handleChange}
+                        >
+                        </input>
+                    </div>
+                    <div className="footer"> 
+                        <button type="submit" className="login-button">Login</button>
                     </div>
                 </form>
             </div>
-            <div className="footer"> 
-                <button type="button" className="login-button">Login</button>
-            </div>
+            
         </div>
 
     }
