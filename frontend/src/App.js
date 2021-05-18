89c17bd757b94377c80cc1c5583f034333e2f4be
zexalo <zexal99@icloud.com>
2021-05-18 19:29:03 +0200

    test sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import './App.css';
 import Alert from 'react-bootstrap/Alert';
 import './Categories/Menu_selection.js';
-
+import SideBar from './Navbar/SideBar.js'
 import { LoginAndRegisterButton, LoginAndRegisterContainer} from "./accueil/loginAndRegister/index"
 
 
 function App() {
   return (
     <div className="App">
+      <SideBar></SideBar>
       <LoginAndRegisterButton
       />
 
     </div>
+  
   );
 }
 
