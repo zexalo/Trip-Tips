ec0433b0dee6324af7f0595d142299e5a1932bac
zexalo <zexal99@icloud.com>
2021-05-21 13:51:29 +0200

    test sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import './App.css';
 import Alert from 'react-bootstrap/Alert';
-import './Categories/Menu_selection.js';
 import SideBar from './Navbar/SideBar.js'
 import Slider from './Categories/Menu_selection.js';
 
 import { LoginAndRegisterButton, LoginAndRegisterContainer} from "./accueil/loginAndRegister/index"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <SideBar/>
+        <Switch>
+            <Route path='/login_register' exact component={LoginAndRegisterButton}/>
+            <Route path='/categories' component={() => <Slider heading="Example Slider"/>}/>
+        </Switch>
+      </Router>
       
-      <LoginAndRegisterButton
-      />
-      <Slider heading="Example Slider"/>
 
     </div>
   
--- frontend/src/Navbar/SideBar.js
+++ frontend/src/Navbar/SideBar.js
@@ -1,63 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react';
+import { ProSidebar, Menu, MenuItem, SubMenu,SidebarHeader,SidebarContent,SidebarFooter } from 'react-pro-sidebar';
+import * as FaIcons from 'react-icons/fa';
+import * as CgIcons from 'react-icons/cg';
+import {Link} from 'react-router-dom';
+
+import './sidebar.scss'
+
+
+
+
+
 
 function SideBar() {
+
+  const [sidebar, setSidebar] = useState(false)
+  const showSidebar = () => setSidebar(!sidebar)
+
     return (
+      <div>
+        <Link to="#">
+          <CgIcons.CgMenuGridO onClick={showSidebar}/>
+        </Link>
+      <ProSidebar className={sidebar ? 'open' : 'close'}>
         
-            <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{width: "280px"}}>
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use href="#bootstrap"></use></svg>
-      <span class="fs-4">Sidebar</span>
-    </a>
-    <hr/>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi me-2" width="16" height="16"><use href="#home"></use></svg>
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-dark">
-          <svg class="bi me-2" width="16" height="16"><use href="#speedometer2"></use></svg>
-          Dashboard
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-dark">
-          <svg class="bi me-2" width="16" height="16"><use href="#table"></use></svg>
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-dark">
-          <svg class="bi me-2" width="16" height="16"><use href="#grid"></use></svg>
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-dark">
-          <svg class="bi me-2" width="16" height="16"><use href="#people-circle"></use></svg>
-          Customers
-        </a>
-      </li>
-    </ul>
-    <hr></hr>
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2"/>
-        <strong>mdo</strong>
-      </a>
-      <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider"/></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
-      </ul>
-    </div>
-  </div>
+        <SidebarHeader >
+        {
+          <Menu iconShape="">
+          <MenuItem icon={<CgIcons.CgMenuGridO/>}>
+            Dashboard
+            <Link to="/" />
+          </MenuItem>
+        </Menu>
+        }
+      </SidebarHeader>
+      <SidebarContent>
+        {/**
+         *  You can add the content of the sidebar ex: menu, profile details, ...
+         */}
+      </SidebarContent>
+      <SidebarFooter>
+        {/**
+         *  You can add a footer for the sidebar ex: copyright
+         */}
+      </SidebarFooter>
+        
+    </ProSidebar></div>
         
     )
 }
+
+
 
 export default SideBar
