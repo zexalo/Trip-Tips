fbcc9924c60b0f114481df7d2e1634282ecba45f
zexalo <zexal99@icloud.com>
2021-05-25 14:38:22 +0200

    rename

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useReducer} from "react";
 import './App.css';
-import Slider from './Categories/Menu_selection.js';
+import Slider from './categories/Menu_selection.js';
 import { LogOut, LoginAndRegisterContainer} from "./accueil/loginAndRegister/index";
 
 import { AuthContext } from "./contexts/AuthContext";
@@ -8,7 +8,7 @@ import {authReducer} from "./hooks/reducers/authReducer";
 import {Token} from "./models/Token";
 import {User} from "./models/User";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SideBar from './Navbar/SideBar'
+import SideBar from './navbar/SideBar'
 import Profil from "./accueil/profil/profil";
 
 
