f21a6307a516f8aa6584526395ac1fd1aba79056
tmax <56720420+TimPionnier@users.noreply.github.com>
2021-06-09 17:14:21 +0200

    update-recomendation-get-request with country name and category id

--- frontend/src/categories/Category.jsx
+++ frontend/src/categories/Category.jsx
@@ -5,33 +5,33 @@ import {AuthContext} from "../contexts/AuthContext";
 import PreviewRecomandation from "../recommendation/Preview";
 
 
-
 const Category = (props) => {
     const {state} = useContext(AuthContext);
     const location = useLocation();
     const [list, setList] = useState([]);
-    const[listIdRecomandationFav, setListIdRecomandationFav] = useState([]);
-    
-    const fetchFavoriteRecommandationID = async () => { 
-        await ApiService.get('/favorites', state).then( (data) =>  setListIdRecomandationFav(data.map( (item) => item.id ) ) )
+    const [listIdRecomandationFav, setListIdRecomandationFav] = useState([]);
+
+    const fetchFavoriteRecommandationID = async () => {
+        await ApiService.get('/favorites', state).then((data) => setListIdRecomandationFav(data.map((item) => item.id)))
         //await ApiService.get('/favorites', state).then( (data) =>  setListIdRecomandationFav(data))
     }
 
     const fetchRecomandations = async () => {
-        await ApiService.get('/recomendations?', state).then((data) =>  setList(data))
+        await ApiService.get(`/recomendations/${location?.state?.country}/${location?.state?.id}`, state).then((data) => setList(data))
     }
 
     useEffect(() => {
+        console.log('id', location?.state?.id)
+        console.log('country', location?.state?.country)
+        console.log('listFavSize', listIdRecomandationFav.length)
 
-        console.log(location?.state?.country)
 
-     
         fetchFavoriteRecommandationID()
-            //.then(() => (console.log(listIdRecomandationFav)));
+            .then(() => (console.log(listIdRecomandationFav)));
 
         fetchRecomandations()
-            //.then(() => (console.log(list)));
-    }, [])
+            .then(() => (console.log(list)));
+    }, [location?.state?.id, location?.state?.country])
 
     const List = () => (
         <ul style={{
@@ -46,22 +46,25 @@ const Category = (props) => {
                 fontSize: 24,
             }}>Recommendations</p>
             {(list || []).map(item => (
-                
+
                 console.log(item),
-                /*
-                console.log(listIdRecomandationFav),
-                console.log(item.id),
-                console.log(listIdRecomandationFav.includes(item.id)),
-                */
-                
-            <PreviewRecomandation title={item.title} content={item.content} id={item.id} isInUserFavorite={listIdRecomandationFav.includes(item.id)} globalRating={item.globalRating} city={item.city} country={item.country} picture={item.picture} price={item.price}  />
+                    /*
+                    console.log(listIdRecomandationFav),
+                    console.log(item.id),
+                    console.log(listIdRecomandationFav.includes(item.id)),
+                    */
+
+                    <PreviewRecomandation title={item.title} content={item.content} id={item.id}
+                                          isInUserFavorite={listIdRecomandationFav.includes(item.id)}
+                                          globalRating={item.globalRating} city={item.city} country={item.country}
+                                          picture={item.picture} price={item.price}/>
             ))}
         </ul>
     );
 
     return (
         <div>
-            <List />
+            {list.length > 0 ? <List/> : <h1 style={{color: 'white'}}>No data</h1>}
         </div>
     );
 };
